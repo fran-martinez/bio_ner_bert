c655bacbb35ecd5bec69ee35c3f31a40f6bf429f
fmartinez01 <nouseforaname>
2020-06-15 15:38:00 +0200

    BertForTokenClassificationCustom documented

--- neural_architectures.py
+++ neural_architectures.py
@@ -4,136 +4,27 @@ from torch.nn.modules.loss import CrossEntropyLoss
 from transformers import BertForTokenClassification
 
 
-class BertForTokenClassificationSimple(BertForTokenClassification):
-    def __init__(self,
-                 config,
-                 freeze_bert=False):
-        super().__init__(config)
-
-        # Freeze bert layers
-        if freeze_bert:
-            for p in self.bert.parameters():
-                p.requires_grad = False
-
-    def forward(self,
-                input_ids=None,
-                attention_mask=None,
-                token_type_ids=None,
-                position_ids=None,
-                head_mask=None,
-                inputs_embeds=None,
-                labels=None,
-                class_weights=None):
-        outputs = self.bert(input_ids,
-                            attention_mask=attention_mask,
-                            token_type_ids=token_type_ids,
-                            position_ids=position_ids,
-                            head_mask=head_mask,
-                            inputs_embeds=inputs_embeds)
-
-        out = self.dropout(outputs[0])
-        logits = self.classifier(out)
-
-        outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
-        if labels is not None:
-            if class_weights is None:
-                loss_fct = CrossEntropyLoss()
-            else:
-                loss_fct = CrossEntropyLoss(weight=class_weights)
-
-            # Only keep active parts of the loss
-            if attention_mask is not None:
-                active_loss = attention_mask.view(-1) == 1
-                active_logits = logits.view(-1, self.num_labels)
-                active_labels = torch.where(active_loss,
-                                            labels.view(-1),
-                                            torch.tensor(loss_fct.ignore_index).type_as(labels))
-
-                loss = loss_fct(active_logits, active_labels)
-            else:
-                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
-            outputs = (loss,) + outputs
-
-        return outputs  # (loss), scores, (hidden_states), (attentions)
-
-
-class BertLstmForTokenClassification(BertForTokenClassification):
-    def __init__(self,
-                 config,
-                 freeze_bert=False,
-                 dropout_lstm=0.05,
-                 num_hidden_units_lstm=256,
-                 num_lstm_layers=1):
-        super().__init__(config)
-
-        if num_lstm_layers == 1:
-            dropout_lstm = .0
-
-        # Let's build a BiLSTM on top of BERT
-        self.lstm = nn.LSTM(self.config.hidden_size,
-                            hidden_size=num_hidden_units_lstm // 2,
-                            num_layers=num_lstm_layers,
-                            dropout=dropout_lstm,
-                            bidirectional=True,
-                            batch_first=True)
-
-        self.linear = nn.Linear(num_hidden_units_lstm, self.config.num_labels)
-
-        # Freeze bert layers
-        if freeze_bert:
-            for p in self.bert_model.parameters():
-                p.requires_grad = False
-
-    def forward(self,
-                input_ids=None,
-                attention_mask=None,
-                token_type_ids=None,
-                position_ids=None,
-                head_mask=None,
-                inputs_embeds=None,
-                labels=None,
-                class_weights=None):
-
-        output_bert = self.bert(input_ids,
-                                attention_mask=attention_mask,
-                                token_type_ids=token_type_ids,
-                                position_ids=position_ids,
-                                head_mask=head_mask,
-                                inputs_embeds=inputs_embeds)
-
-        out_lstm, _ = self.lstm(output_bert[0])
-        out_lstm = self.dropout(out_lstm)
-        logits = self.linear(out_lstm)
-
-        output_bert = (logits,) + output_bert[2:]  # add hidden states and attention if they are here
-        if labels is not None:
-            if class_weights is None:
-                loss_fct = CrossEntropyLoss()
-            else:
-                loss_fct = CrossEntropyLoss(weight=class_weights)
-
-            # Only keep active parts of the loss
-            if attention_mask is not None:
-                active_loss = attention_mask.view(-1) == 1
-                active_logits = logits.view(-1, self.num_labels)
-                active_labels = torch.where(active_loss,
-                                            labels.view(-1),
-                                            torch.tensor(loss_fct.ignore_index).type_as(labels))
-
-                loss = loss_fct(active_logits, active_labels)
-            else:
-                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
-            output_bert = (loss,) + output_bert
-
-        return output_bert  # (loss), scores, (hidden_states), (attentions)
-
-
 class BertForTokenClassificationCustom(nn.Module):
     def __init__(self,
                  model_name: str,
                  num_labels: int,
                  hidden_dropout_prob: float,
                  attention_probs_dropout_prob: float):
+        """
+        This model is a replica of BertForTokenClassification class but instead of being
+        a subclass of `PreTrainedModel` (transformers library) it is a subclass of `nn.Module`
+        from Pytorch. In fact, `BertForTokenClassification` is instantiated through `from_pretrained`
+        within the `init` method.
+        The only difference in functionallity is within the `forward` method.
+        Here the output of BERT model is reshaped to be directly compatible with
+        `nn.module.CrossEntropyLoss` (batch_size, num_classes, sequence_len). This is done in
+        order to make compatible this BERT model with the `torch_lr_finder` library.
+        Args:
+            model_name (`str`): model name as expected in Transformers library
+            num_labels (`int`): number of labels
+            hidden_dropout_prob(`float`): droput
+            attention_probs_dropout_prob (`float`): dropout in attention
+        """
         super().__init__()
         self.bert = BertForTokenClassification.from_pretrained(model_name,
                                                                num_labels=num_labels,
